b0ab27c7e6c2f72225085e4c891443a55395b7ca
slickFix <sidd.200894@gmail.com>
2019-01-23 22:05:57 +0530

    DS_InsertionSort_rec adding comments

--- DS_ALGO/InsertionSort_rec.py
+++ DS_ALGO/InsertionSort_rec.py
@@ -12,11 +12,12 @@ def InsertionSort_rec(li,k):
     
     if k>1:
         InsertionSort_rec(li,k-1)
-        li[:k] = isort(li[:k],k)  
+        li[:k] = isort(li[:k],k)# using "li[:k] =" as li and li_i don't share memory
 
 
 def isort(li_i,k):
     
+    # list li_i memory is different than the li as it's a slice of li
     pos = k-1 
     while pos >0 and li_i[pos-1]>li_i[pos]:
         li_i[pos-1],li_i[pos] = li_i[pos],li_i[pos-1]
