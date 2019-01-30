8d4a7ffe3c2a6cdc6b57182aa6197d6e107e50be
slickFix <sidd.200894@gmail.com>
2019-01-30 21:05:04 +0530

    adding graph.py

--- Building TensorFlow API from Scratch/graph.py
+++ Building TensorFlow API from Scratch/graph.py
@@ -6,3 +6,13 @@ Created on Wed Jan 30 20:06:34 2019
 @author: siddharth
 """
 
+class Graph():
+    def __init__(self):
+        self.operations = []
+        self.placeholders = []
+        self.variables = []
+        self.constants = [] 
+        
+    def as_default(self):
+        global default_graph
+        _default_graph = self
