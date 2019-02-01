920e3a3b50bf5a60adae84afcf2cfcfade539a14
slickFix <sidd.200894@gmail.com>
2019-02-01 21:26:38 +0530

    adding path to the operation.py file

--- Building TensorFlow API from Scratch/operation.py
+++ Building TensorFlow API from Scratch/operation.py
@@ -6,6 +6,13 @@ Created on Thu Jan 31 21:05:57 2019
 @author: siddharth
 """
 
+import os
+os.chdir('/home/siddharth/workspace-python/Python/Building TensorFlow API from Scratch/')
+path  = os.getcwd()
+
+import sys
+sys.path.append(path)
+
 class Operation:
     def __init__(self,input_nodes = None):
         self.input_nodes = input_nodes
