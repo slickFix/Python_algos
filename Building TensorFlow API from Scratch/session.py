daf746ea333aea527095c43aa766a1c7c866a0e4
slickFix <sidd.200894@gmail.com>
2019-02-07 20:25:01 +0530

    adding session.py

---
+++ Building TensorFlow API from Scratch/session.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  7 20:12:42 2019
+
+@author: siddharth
+"""
+
+
+import os
+os.chdir('/home/siddharth/workspace-python/Python/Building TensorFlow API from Scratch/')
+
+path  = os.getcwd()
+
+import sys
+sys.path.append(path)
+
+class Session():
+    
