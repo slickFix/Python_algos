08453c99d4ecf197127c26165f53369369981ca1
slickFix <sidd.200894@gmail.com>
2019-02-12 20:41:46 +0530

    DS_N_queens initial commit

---
+++ DS_ALGO/N_queens.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 12 20:37:05 2019
+
+@author: siddharth
+"""
+
+
+
+if __name__ == '__main__':
+    
+    n = input('Enter the number of queens :')
+    
