6f1e7cfdb3e1015e82ffdf9afc943cb8cad66e71
slickFix <sidd.200894@gmail.com>
2019-02-14 19:12:34 +0530

    DS_N_queens adding initializeBoard function

--- DS_ALGO/N_queens.py
+++ DS_ALGO/N_queens.py
@@ -7,8 +7,21 @@ Created on Tue Feb 12 20:37:05 2019
 """
 
 
+def initializeBoard(board,n):
+    for i in ['queen','row','col','nwtose','swtone']:
+        board[i]={}
+    for i in range(n):
+        board['queen'][i] = -1
+        board['row'][i]=0
+        board['col'][i]=0
+    for i in range(-(n-1),n):
+        board['nwtose'][i] = 0
+    for i in range(2*n-1):
+        board['swtone'][i] = 0
+
 
 if __name__ == '__main__':
     
     n = input('Enter the number of queens :')
-    
+    board = {}
+    initializeBoard(board,n)
