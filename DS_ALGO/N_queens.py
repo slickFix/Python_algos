0b7c59656c2e4c6a004d834759dcfa369163a3d0
slickFix <sidd.200894@gmail.com>
2019-02-18 18:56:54 +0530

    DS_N_queens adding free function

--- DS_ALGO/N_queens.py
+++ DS_ALGO/N_queens.py
@@ -23,8 +23,15 @@ def printBoard(board):
     for row in sorted(board['queen'].keys()):
         print((row,board['queen'][row]))
 
+def free(i,j,board):
+    return (board['queen'][i]==-1 and board['row'][i]==0 and board['col'][i]==0 \
+            and board['nwtose'][i-j]==0 and board['swtone'][i+j]==0)
+    
+    
+    
 if __name__ == '__main__':
     
     n = input('Enter the number of queens :')
     board = {}
     initializeBoard(board,n)
+    
