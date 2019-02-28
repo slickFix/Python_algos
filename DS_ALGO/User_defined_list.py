c162b275de443581663645afbcc121b02cf22fda
slickFix <sidd.200894@gmail.com>
2019-02-28 20:15:08 +0530

    DS_User_defined_list initial commit

---
+++ DS_ALGO/User_defined_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 28 20:08:59 2019
+
+@author: siddharth
+"""
+
+class Li_node:
+    def __init__(self,value=None):
+        self.value = value
+        self.next = None
+    
+    
+    
+    
+if __name__ == '__main__':
+    
