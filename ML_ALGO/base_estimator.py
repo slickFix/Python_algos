01cca9c58e3015573c7ca789dead9f8ca1b87b8a
slickFix <sidd.200894@gmail.com>
2019-05-31 10:29:46 +0530

    implemented _setup_input() in base_estimator.py

--- ML_ALGO/base_estimator.py
+++ ML_ALGO/base_estimator.py
@@ -6,3 +6,53 @@ Created on Fri May 31 09:41:46 2019
 @author: siddharth
 """
 
+import numpy as np
+
+class BaseEstimator(object):
+    
+    x= None
+    y = None
+    y_required = True
+    fit_required = True
+    
+    def _setup_input(self,x,y=None):
+        """Ensure inputs to an estimator are in the expected (size,dim etc) format.
+        Ensures X and y are stored as numpy ndarrays by converting from an
+        array-like object if necessary. Enables estimators to define whether
+        they require a set of y target values or not with y_required, e.g.
+        kmeans clustering requires no target labels and is fit against only X.
+        Parameters
+        ----------
+        X : array-like
+            Feature dataset.
+        y : array-like
+            Target values. By default is required, but if y_required = false
+            then may be omitted.
+        """
+        
+        if not isinstance(x,np.ndarray):
+            x = np.array(x)
+        
+        if x.size() == 0 :
+            raise ValueError('Number of features must be >0')
+            
+        if x.ndim == 1:
+            self.n_samples,self.n_features = 1,x.shape
+        else:
+            self.n_samples,self.n_features = x.shape[0],len(x.shape[1:])
+        
+        self.x = x
+        
+        if self.y_required:
+            
+            if y is None:
+                raise ValueError('Missing required argument y')
+            
+            if not isinstance(y,np.ndarray):
+                y = np.array(y)
+            
+            if y.size() ==0:
+                raise ValueError('Number of targets must be >0')
+                
+        self.y = y
+        
