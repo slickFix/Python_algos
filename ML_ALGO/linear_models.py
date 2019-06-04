bb09b1052d9b680aaacde1132761bb1bdc632857
slickFix <sidd.200894@gmail.com>
2019-06-04 18:55:30 +0530

    completed the implementation of  BasicRegression_Wrapper class

--- ML_ALGO/linear_models.py
+++ ML_ALGO/linear_models.py
@@ -49,8 +49,87 @@ class BasicRegression_Wrapper(BaseEstimator):
         self.theta = []
         self.n_samples,self.n_features = None,None
         self.cost_func = None
-        
     
+    def _loss(self,wt):
+        raise NotImplementedError
         
+    def init_cost(self):
+        raise NotImplementedError
         
+    def _add_penalty(self,loss,wt):
+        "Applying regularisation to the cost function i.e. _loss"
+        
+        if self.penalty=='l1':
+            
+            # not including the bias term for regularisation
+            loss+= self.C * np.abs(wt[1:]).sum()
+        
+        if self.penalty=='l2':
+            
+            # not including the bias term for regularisation
+            loss+= (0.5*self.C) * (wt[1:] ** 2).sum()
+            
+        return loss
+    
+    def _calc_cost(self,x,y,theta):
+        
+        predictions = x.dot(theta)
+        error = self.cost_func(predictions,y)
+        return error
+    
+    def fit(self,x,y):
+        
+        self._setup_input(x,y)
+        self.init_cost()
+        self.n_samples,self.n_features = x.shape
+        
+        # initialize weights and bias terms
+        self.theta = np.random.normal(size=(self.n_features+1),scale = 0.5)
+        
+        # adding the intercept term to the input i.e. x
+        self.x = self._add_intercept(self.x)
+        
+        self._train()
+    
+    @staticmethod
+    def _add_intercept(x):
+        first_col = np.ones([x.shape[0],1])
+        return np.concatenate([first_col,x],axis=1)
+    
+    def _train(self):
+        self.theta,self.errors = self.gradient_descent()
+        logging.info(' Theta : %s '%self.theta.flatten())
+        
+    def _predict(self,x=None):
+        
+        x = self._add_intercept(x)
+        return x.dot(self.theta)
+    
+    def _gradient_descent(self):
+        theta = self.theta
+        
+        errors = [self._calc_cost(self.x,self.y,self.theta)]
+        
+        # "defining" the derivative of the loss function
+        grad_calc_def = grad(self._loss)
+        
+        for i in range(1,self.max_iters+1):
+            
+            # calculates the gradients for all the inputs to the _loss function
+            # in this case it is all the weights gradients being calculated
+            
+            grad = grad_calc_def(theta)
+            theta-= self.lr * grad
+            
+            errors.append(self._calc_cost(self.x,self.y,theta))
+            
+            logging.info('Iteration: %s Train cost : %s' %(i,errors[i]))
+            
+            error_diff = np.linalg.norm(errors[i-1]-errors[i])
+            
+            if error_diff < self.error_tolerance:
+                logging.info('Convergence reached')
+                break
+        
+        return theta,errors
         
