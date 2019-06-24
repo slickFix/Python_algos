9acfcbb28c1e16150d4c03de8b15345d825b272b
slickFix <sidd.200894@gmail.com>
2019-06-24 14:56:39 +0530

    adding split_target() in ensemble_utils.py

--- ML_ALGO/ensemble_utils.py
+++ ML_ALGO/ensemble_utils.py
@@ -6,3 +6,13 @@ Created on Thu Jun 20 11:41:55 2019
 @author: siddharth
 """
 
+import numpy as np
+
+def split_target(x,y,value):
+    
+    left_mask = x<value
+    right_mask = x >= value
+    
+    return y[left_mask],y[right_mask]
+
+
