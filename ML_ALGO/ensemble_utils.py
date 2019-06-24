82d986c1b9bdc39dee086d6728e54cc65f2eb8e2
slickFix <sidd.200894@gmail.com>
2019-06-24 17:02:40 +0530

    added split_dataset and get_split_mask functions in ensemble_utils.py

--- ML_ALGO/ensemble_utils.py
+++ ML_ALGO/ensemble_utils.py
@@ -16,3 +16,27 @@ def split_target(x,y,value):
     return y[left_mask],y[right_mask]
 
 
+def get_split_mask(x,column,value):
+    '''
+    getting split mask for a particular column
+    '''
+    
+    left_mask = x[:,column]<value
+    right_mask = x[:,column]>=value
+    return left_mask,right_mask
+
+def split_dataset(x,target,column,value,return_x=True):
+    
+    left_mask , right_mask = get_split_mask(x,column,value)
+    
+    left,right = {},{}
+    
+    # target is the 'y' in a dictonary form
+    for key in target.keys():
+        left[key] = target[key][left_mask]
+        right[key] = target[key][right_mask]
+        
+    if return_x:
+        return x[left_mask],x[right_mask],left,right
+    else:
+        return left,right
