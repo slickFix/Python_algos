74b5c0e3af1cc1455bc4a39c7465c376a48dd6cb
slickFix <sidd.200894@gmail.com>
2019-07-01 18:33:52 +0530

    importing libraries in random_forest.py

--- ML_ALGO/random_forest.py
+++ ML_ALGO/random_forest.py
@@ -6,3 +6,8 @@ Created on Fri Jun 21 15:25:03 2019
 @author: siddharth
 """
 
+import numpy as np
+from base_estimator import BaseEstimator
+from decision_tree import Decision_tree
+from ensemble_utils import mse_criterion,information_gain
+
