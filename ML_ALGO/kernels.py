3a901fa648bfe1c5cd53dac40ebe65b77dfe8e7e
slickFix <sidd.200894@gmail.com>
2019-07-10 23:06:03 +0530

    Added linear kernel to the kernels.py

--- ML_ALGO/kernels.py
+++ ML_ALGO/kernels.py
@@ -6,3 +6,17 @@ Created on Wed Jul 10 22:58:36 2019
 @author: siddharth
 """
 
+import numpy as np
+import scipy.spatial.distance  as dist
+
+
+''' Defining vararious types of kernels for svm '''
+
+class Linear:
+    
+    def __call__(self,x,y):
+        return np.dot(x,y.T)
+    
+    def __repr__(self):
+        return 'Linear Kernel'
+
