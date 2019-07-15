ff3405cb827144a8944b78165507f599fc533453
slickFix <sidd.200894@gmail.com>
2019-07-15 19:19:44 +0530

    implemented SVM class and constructor in svm.py

--- ML_ALGO/svm.py
+++ ML_ALGO/svm.py
@@ -11,3 +11,43 @@ from kernels import Linear
 
 import numpy as np
 
+np.random.seed(2019)
+
+'''
+SVM optimization using SMO(sequential minimal optimization) algorithm
+
+reference:- http://cs229.stanford.edu/materials/smo.pdf
+
+'''
+
+class SVM(BaseEstimator):
+    
+    def __init__(self,C=1.0,tol=1e-3,kernel = None,max_iter = 100):
+        
+        ''' Support vector machine implementation using simplified SMO 
+        
+        parameters
+        --------------
+        
+        C: float,default 1.0 [penalty]
+        tol: float,default 1e-3 [tolerance]
+        kernel: Kernel object
+        max_iter: int, default 100 [maximum iteration]
+        
+        '''
+        
+        self.C = C
+        self.tol = tol
+        self.max_iter = max_iter
+        
+        if kernel is None:
+            self.kernel = Linear()
+        
+        else:
+            self.kernel = kernel
+            
+        self.b = 0
+        self.K = None
+        self.alpha = None
+            
+        
