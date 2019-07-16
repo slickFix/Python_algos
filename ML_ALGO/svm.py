b508b739fd4a116f259b47548329f7202e4c0399
slickFix <sidd.200894@gmail.com>
2019-07-16 10:29:24 +0530

    Added _train() function in svm.py

--- ML_ALGO/svm.py
+++ ML_ALGO/svm.py
@@ -72,12 +72,71 @@ class SVM(BaseEstimator):
         self.sv_idx = np.arange(0,self.n_samples)
         
         self._train()
+        
+            
+    def _train(self):
+        
+        iters = 0 
+        
+        while iters < self.max_iter:
+            iters+=1
+            
+            alpha_prev = np.copy(self.alpha)
+            
+            for j in range(self.n_samples):
+                
+                # Picking random i
+                i = self.random_index(j)
+                
+                eta = 2.0 * self.K[i,j] - self.K[i,i] - self.K[j,j]
+                
+                if eta >= 0:
+                    continue
+                
+                L,H = self._find_bounds(i,j)
+                
+                # Error for current examples
+                e_i,e_j =self._error(i),self._error(j)
+                
+                # save old alphas
+                alpha_io, alpha_jo = self.alpha[i] , self.alpha[j]
+                
+                # update alpha
+                self.alpha[j] -= (self.y[j]* (e_i-e_j))/eta
+                self.alpha[j] = self._clip(self.alpha[j],L,H)
+                
+                self.alpha[i] = self.alpha[i] + self.y[i]*self.y[j]*(alpha_jo-self.alpha[j])
+                
+                # Find intercept
+                b1 = self.b - e_i - self.y[i] * (self.alpha[i] - alpha_jo) * self.K[i, i] - \
+                     self.y[j] * (self.alpha[j] - alpha_jo) * self.K[i, j]
+                b2 = self.b - e_j - self.y[j] * (self.alpha[j] - alpha_jo) * self.K[j, j] - \
+                     self.y[i] * (self.alpha[i] - alpha_io) * self.K[i, j]
+                     
+                if 0 < self.alpha[i] < self.C:
+                    self.b = b1
+                elif 0 < self.alpha[j] < self.C:
+                    self.b = b2
+                else:
+                    self.b = 0.5 * (b1 + b2)
+                    
+            # checking for convergence
+            diff = np.linalg.norm(self.alpha-alpha_prev)
+            
+            if (diff<self.tol):
+                break
+        
+        print('Convergence reached after {} iterations'.format(iters))
+        
+        # updating support vector index
+        self.sv_idx = np.where(self.alpha > 0 )[0]
+                
     
     def _predict_row(self,x):
         
         '''
-        k_v : is m*1 array formed after dot product of test(t) vector with all
-              train(set) vector
+        k_v : is no_of_support_vectors*1 array formed after dot product of test(t) vector with all
+              train_set_support_vectors
             
         '''
         k_v = self.kernel(self.x[self.sv_idx],x)
